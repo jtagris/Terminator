debbc544a32b0c017035c6777080e9cd0fef2409
p3autret <p3autret@enib.fr>
2024-02-26 22:54:33 +0100

    First Commit

---
+++ Scenario.java
@@ -0,0 +1,12 @@
+package TERMINATOR;
+
+public class Scenario 
+{
+    public static void main(String[] argv)
+        {
+            System.out.println("Dialogue entre machines....");
+
+            Terminator t1 = new Terminator(101);
+            t1.SePresenter();
+        }    
+}
---
+++ Terminator.java
@@ -0,0 +1,24 @@
+package TERMINATOR;
+
+class Terminator 
+{
+    private int ID;
+
+    public Terminator(int id)
+        {
+            this.ID = id;
+        }
+
+    public void SePresenter()
+        {
+            System.out.println("Je suis un Terminator et mon numéro de série est ..." + this.ID);
+        }
+
+    public void Termine()
+        {
+            System.out.println("Vous etes terminés!!");
+        }
+}
+
+
+
